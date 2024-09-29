ffaef0c1a05d0759d9615c316147489c0be5b681
dogpatch626 <djpepin1299@gmail.com>
2024-09-28 23:14:08 -0400

    Initial Commit

---
+++ commands/commands.go
@@ -0,0 +1,11 @@
+package commands
+
+import "github.com/fatih/color"
+
+func Info() {
+	color.Red("CodexCli is a in development cli made in go as a project to learn what is even going on with this lang.")
+}
+
+func DoSomething() {
+	color.Red("do something")
+}
--- main.go
+++ main.go
@@ -2,38 +2,44 @@ package main
 
 import (
 	"CodexCli/banner"
+	"CodexCli/commands"
 	"bufio"
 	"fmt"
 	"os"
-
-	"github.com/fatih/color"
 )
 
+type value struct {
+	Value func()
+}
+
+func DoSomething() {
+	fmt.Println("does Something")
+
+}
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
-
 	banner.Banner()
+
+	m := make(map[string]func())
+	m[":do"] = commands.DoSomething
+	m[":info"] = commands.Info
 	var input []string
 	for scanner.Scan() {
 
-		// fmt.Println(" typed ", scanner.Text() == string('.'))
-
-		// in := bufio.NewReader(os.Stdin)
 		in := scanner.Text()
 		if in == string(":q") {
 			return
 		}
-		// for idk := 0; idk < 5; idk++ {
-		// 	fmt.Println("Weeeeeee")
-		// }
+
 		input = append(input, in)
 
 		switch in {
 		case string(":info"):
-			color.Red("CodexCli is a in development cli made in go as a project to learn what is even going on with this lang.")
+			m[":info"]()
+		case string(":do"):
+			m[":do"]()
 		}
-		// s = strings.TrimSpace(s)
 
 		fmt.Println(input)
 
