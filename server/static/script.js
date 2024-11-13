c43e7c044d953589d3e84b6834b45bcfabe90fac
dogpatch626 <djpepin1299@gmail.com>
2024-11-13 09:31:52 -0500

    remove server

--- server/static/script.js
+++
@@ -1,9 +0,0 @@
-importScripts("wasm_exec.js")
-const go = new Go();
-
-WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
-
-    go.run(result.instance)
-
-    console.log("posted")
-});
