f42f71e6359e359107f8b45b04525fd8c8693cc8
dogpatch626 <djpepin1299@gmail.com>
2024-11-13 10:40:58 -0500

    add env reading

--- main.go
+++ main.go
@@ -4,14 +4,39 @@ import (
 	"CodexCli/banner"
 	TextProcessor "CodexCli/textprocessor"
 	"bufio"
+	"fmt"
+	"log"
 	"os"
 	"strings"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 	banner.Banner()
+
+	err := godotenv.Load()
+
+	if err != nil {
+		f, CreationError := os.Create(".env")
+		if CreationError != nil {
+			log.Fatal("Error loading and writing .env file")
+		}
+		defer f.Close()
+	}
+
+	var myEnv map[string]string
+	myEnv, ReadError := godotenv.Read()
+
+	if ReadError != nil {
+		log.Fatal("Error reading .env")
+	}
+
+	if len(myEnv) == 0 {
+		fmt.Println("your .env is empty")
+	}
 
 	for scanner.Scan() {
 
